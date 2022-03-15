167fb3157b8b4a6f311ca52e11833b484d09dd16
David Chau <david.chau@live.ca>
2022-03-14 20:03:58 -0400

    Add initial files

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import {Link} from "@mui/material";
+import FreeSolo from './components/FreeSolo';
+import links from './links.json';
+import './App.css';
+
+const App = () => {
+    const [text, setText] = useState<string>('');
+
+    return (
+        <div className="link-generator">
+          <FreeSolo
+              text={text}
+              setText={setText}
+          />
+            {!!text && Object
+                .entries(links)
+                .map(([key, value]) => <Link key={key} href={`${value}${text}`}>{key}</Link>)}
+        </div>
+    );
+}
+
+
+export default App;
